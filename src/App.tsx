2f55a014211044ff166b8d600003075d95d46be1
KyMback <Seregick@hotmail.com>
2021-10-29 11:00:23 +0300

    Added dropzone configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import { useEffect, useRef, useState } from "react";
 import { observer } from "mobx-react-lite";
 import { AppStore } from "stores/AppStore";
+import { useDropzone } from "react-dropzone";
 
 export const App = observer(() => {
   const ref = useRef<HTMLCanvasElement>(null);
   const [store] = useState(new AppStore(ref));
+  const { getInputProps, open } = useDropzone({
+    accept: ["image/*"],
+    multiple: false,
+    onDrop: store.setFile,
+  });
 
   useEffect(() => store.dispose, [store]);
 
@@ -12,7 +18,10 @@ export const App = observer(() => {
     <div>
       <div style={{ display: "flex", flexDirection: "column" }}>
         <canvas ref={ref} style={{ width: "300px" }} />
-        <input type="file" onChange={(e) => store.setFile(e.target.files)} />
+        <div>
+          <button onClick={open}>Upload</button>
+          <input {...getInputProps()} />
+        </div>
       </div>
       <div>
         <textarea
